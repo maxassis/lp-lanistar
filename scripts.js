87f4a26190e0cef5c1ef2913fd3cf87b2e1f92b9
max <max.assis@chatguru.com.br>
2023-05-30 15:40:32 -0300

    feat(app): corrigindo o menu mobile

--- scripts.js
+++ scripts.js
@@ -13,3 +13,22 @@ function showMenu() {
     openBtn.style.display = isOpenBtnDisplayed;
     document.body.style.overflow = isBodyOverflowHidden;
   }
+
+
+  const items = document.querySelectorAll(".section-mobile__item");
+
+  function activeItem() {
+    if (this.classList.contains("section-mobile__item--active")) {
+      return;
+    }
+  
+    items.forEach((item) => {
+      item.classList.remove("section-mobile__item--active");
+    });
+  
+    this.classList.add("section-mobile__item--active");
+  }
+  
+  items.forEach((item) => {
+    item.addEventListener("click", activeItem);
+  })
